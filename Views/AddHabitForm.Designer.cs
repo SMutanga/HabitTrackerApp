8c771fd78217f4f9c4eb1be6d405127d79922a53
Sannah Penelope Azuh Mutanga <penelopemutanga@outlook.com>
2025-11-26 18:10:47 -0600

    Added GUI for AddHabitForm

--- Views/AddHabitForm.Designer.cs
+++ Views/AddHabitForm.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHabitName = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Habit Name:";
+            // 
+            // txtHabitName
+            // 
+            this.txtHabitName.Location = new System.Drawing.Point(162, 13);
+            this.txtHabitName.Name = "txtHabitName";
+            this.txtHabitName.Size = new System.Drawing.Size(100, 20);
+            this.txtHabitName.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(115, 77);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // AddHabitForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddHabitForm";
+            this.ClientSize = new System.Drawing.Size(384, 161);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtHabitName);
+            this.Controls.Add(this.label1);
+            this.Name = "AddHabitForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add Habit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHabitName;
+        private System.Windows.Forms.Button btnSave;
     }
 }
